50ee7b3f76f8503f6eac3ba42e5dc5038913b7f5
zhangshuang <zhangshuang@fangmaster.com>
2018-02-24 12:04:28 +0800

    Initial Commit

---
+++ BabyCustomScreening/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BabyCustomScreening
+//
+//  Created by 北鼻爽 on 2018/2/24.
+//  Copyright © 2018年 北鼻爽. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
